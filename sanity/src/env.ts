32097e035dcb57a020134222d55426e4d2901545
junghyeonsu <jung660317@naver.com>
2024-01-30 17:20:07 +0900

    feat(sanity): remove env

--- sanity/src/env.ts
+++ sanity/src/env.ts
@@ -1,15 +1,4 @@
-const sanityProjectId = process.env.SANITY_STUDIO_PROJECT_ID;
-const sanityDataset = process.env.SANITY_STUDIO_DATASET;
-
-if (!sanityProjectId) {
-  throw new Error("Missing SANITY_STUDIO_PROJECT_ID environment variable");
-}
-
-if (!sanityDataset) {
-  throw new Error("Missing SANITY_STUDIO_DATASET environment variable");
-}
-
 export const sanityConfig = {
-  projectId: sanityProjectId,
-  dataset: sanityDataset,
+  projectId: "lzv0s5lm",
+  dataset: "production",
 };
